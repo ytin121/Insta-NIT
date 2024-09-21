34a9c38e564490185e2310a1fb75d7003ab83580
ytin121 <128726040+ytin121@users.noreply.github.com>
2024-09-22 01:50:17 +0530

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -19,26 +19,7 @@ const products = [
       "apparel"
     ]
   },
-  {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c0",
-    image: "https://media.licdn.com/dms/image/D4D03AQGNpdpUca8UAg/profile-displayphoto-shrink_200_200/0/1676016290732?e=2147483647&v=beta&t=5lJB8HN6hq0rfCynYvHLD71ESG9JVouysGMU-ctif_I",
-    name: "Jasjot Singh",
-    rating: {
-      followers: 4.5,
-      connections: 87
-    },
-    work: "Student @ National Institute of Technology Hamirpur",
 
-    linkdin: "https://in.linkedin.com/in/arora-jasjot",
-    insta: "https://www.instagram.com/arora_jasjot",
-    gmail: "20bme024@nith.ac.in",
-    github: "https://www.youtube.com/@ytintech5468",
-    twitter: "https://www.youtube.com/@ytintech5468", keywords: [
-      "socks",
-      "sports",
-      "apparel"
-    ]
-  },
   {
     id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     image: "https://media.licdn.com/dms/image/D5603AQEV4GnhP9PGpw/profile-displayphoto-shrink_100_100/0/1692099996356?e=2147483647&v=beta&t=rFFnjjCZdadxYtdnsX0PY5SFvwqzUbr9a5YIeTyaQAo",
@@ -81,7 +62,7 @@ const products = [
   {
     id: "54e0eccd-8f36-462b-b68a-8182611d9add",
     image: "https://images.unsplash.com/photo-1499952127939-9bbf5af6c51c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
-    name: "ter - Black",
+    name: "mishal",
     rating: {
       followers: 5,
       connections: 2197
@@ -101,7 +82,7 @@ const products = [
   {
     id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
     image: "https://images.unsplash.com/photo-1504593811423-6dd665756598?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
-    name: "Dinner Plate Set",
+    name: "Downy",
     rating: {
       followers: 4,
       connections: 37
@@ -121,7 +102,7 @@ const products = [
   {
     id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
     image: "https://images.unsplash.com/photo-1599566150163-29194dcaad36?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
-    name: "6-Piece Nonst",
+    name: "peter",
     rating: {
       followers: 4.5,
       connections: 175
@@ -140,7 +121,7 @@ const products = [
   {
     id: "e43638ce-6aa0-4b85-4b27f-e1d07eb678c6",
     image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww",
-    name: "Black andletic Cotton Socks - 6 Pairs",
+    name: "Kraven",
     rating: {
       followers: 4.5,
       connections: 87
@@ -160,7 +141,163 @@ const products = [
   {
     id: "15b6fc6f-327a-4ec4-8946f-4864349e85a3d",
     image: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVyc29ufGVufDB8fDB8fHww",
-    name: "Interasketball",
+    name: "lewis",
+    rating: {
+      followers: 4,
+      connections: 127
+    },
+    work: "",
+
+    linkdin: "",
+    insta: "",
+    gmail: "",
+    github: "https://www.youtube.com/@ytintech5468",
+    twitter: "https://www.youtube.com/@ytintech5468", keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },
+  {
+    id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+    image: "https://images.unsplash.com/photo-1504593811423-6dd665756598?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
+    name: "Downy",
+    rating: {
+      followers: 4,
+      connections: 37
+    },
+    work: "",
+
+    linkdin: "",
+    insta: "",
+    gmail: "",
+    github: "https://www.youtube.com/@ytintech5468",
+    twitter: "https://www.youtube.com/@ytintech5468", keywords: [
+      "plates",
+      "kitchen",
+      "dining"
+    ]
+  },
+  {
+    id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
+    image: "https://images.unsplash.com/photo-1599566150163-29194dcaad36?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
+    name: "peter",
+    rating: {
+      followers: 4.5,
+      connections: 175
+    },
+    work: "",
+
+    linkdin: "",
+    insta: "",
+    gmail: "",
+    github: "https://www.youtube.com/@ytintech5468",
+    twitter: "https://www.youtube.com/@ytintech5468", keywords: [
+      "kitchen",
+      "cookware"
+    ]
+  },
+  {
+    id: "e43638ce-6aa0-4b85-4b27f-e1d07eb678c6",
+    image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww",
+    name: "Kraven",
+    rating: {
+      followers: 4.5,
+      connections: 87
+    },
+    work: "",
+
+    linkdin: "",
+    insta: "",
+    gmail: "",
+    github: "https://www.youtube.com/@ytintech5468",
+    twitter: "https://www.youtube.com/@ytintech5468", keywords: [
+      "socks",
+      "sports",
+      "apparel"
+    ]
+  },
+  {
+    id: "15b6fc6f-327a-4ec4-8946f-4864349e85a3d",
+    image: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVyc29ufGVufDB8fDB8fHww",
+    name: "lewis",
+    rating: {
+      followers: 4,
+      connections: 127
+    },
+    work: "",
+
+    linkdin: "",
+    insta: "",
+    gmail: "",
+    github: "https://www.youtube.com/@ytintech5468",
+    twitter: "https://www.youtube.com/@ytintech5468", keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },
+  {
+    id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+    image: "https://images.unsplash.com/photo-1504593811423-6dd665756598?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
+    name: "Downy",
+    rating: {
+      followers: 4,
+      connections: 37
+    },
+    work: "",
+
+    linkdin: "",
+    insta: "",
+    gmail: "",
+    github: "https://www.youtube.com/@ytintech5468",
+    twitter: "https://www.youtube.com/@ytintech5468", keywords: [
+      "plates",
+      "kitchen",
+      "dining"
+    ]
+  },
+  {
+    id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
+    image: "https://images.unsplash.com/photo-1599566150163-29194dcaad36?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
+    name: "peter",
+    rating: {
+      followers: 4.5,
+      connections: 175
+    },
+    work: "",
+
+    linkdin: "",
+    insta: "",
+    gmail: "",
+    github: "https://www.youtube.com/@ytintech5468",
+    twitter: "https://www.youtube.com/@ytintech5468", keywords: [
+      "kitchen",
+      "cookware"
+    ]
+  },
+  {
+    id: "e43638ce-6aa0-4b85-4b27f-e1d07eb678c6",
+    image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww",
+    name: "Kraven",
+    rating: {
+      followers: 4.5,
+      connections: 87
+    },
+    work: "",
+
+    linkdin: "",
+    insta: "",
+    gmail: "",
+    github: "https://www.youtube.com/@ytintech5468",
+    twitter: "https://www.youtube.com/@ytintech5468", keywords: [
+      "socks",
+      "sports",
+      "apparel"
+    ]
+  },
+  {
+    id: "15b6fc6f-327a-4ec4-8946f-4864349e85a3d",
+    image: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVyc29ufGVufDB8fDB8fHww",
+    name: "lewis",
     rating: {
       followers: 4,
       connections: 127
@@ -179,7 +316,7 @@ const products = [
   {
     id: "83d4ca15-0f35-48f5-b7a3-14ea210004f2e",
     image: "https://images.unsplash.com/photo-1491349174775-aaafddd81942?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
-    name: "Adon T-Shirt - 2 Pack",
+    name: "Adon",
     rating: {
       followers: 4.5,
       connections: 56
@@ -197,11 +334,70 @@ const products = [
     ],
     type: "clothing",
     sizeChartLink: "images/clothing-size-chart.png"
+  }, {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: "https://media.licdn.com/dms/image/D5603AQEqve0H21guvw/profile-displayphoto-shrink_200_200/0/1699944145442?e=2147483647&v=beta&t=Obtw8Q7KNcONEy4zby9QWb6seijPStrv5wy1ynbSK10",
+    name: "Yatin",
+    rating: {
+      followers: 4.5,
+      connections: 87
+    },
+    work: "Attended National Institute of Technology Hamirpur-Alumni",
+
+    linkdin: "https://in.linkedin.com/in/yatin-b7a952296",
+    insta: "https://www.instagram.com/_yatin.8t35",
+    gmail: "22bcs121@nith.ac.in",
+    github: "https://www.youtube.com/@ytintech5468",
+    twitter: "https://www.youtube.com/@ytintech5468", keywords: [
+      "socks",
+      "sports",
+      "apparel"
+    ]
+  },
+
+  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "https://media.licdn.com/dms/image/D5603AQEV4GnhP9PGpw/profile-displayphoto-shrink_100_100/0/1692099996356?e=2147483647&v=beta&t=rFFnjjCZdadxYtdnsX0PY5SFvwqzUbr9a5YIeTyaQAo",
+    name: "Vishal Meena",
+    rating: {
+      followers: 4,
+      connections: 127
+    },
+    work: "Attended National Institute of Technology Hamirpur-Alumni",
+
+    linkdin: "",
+    insta: "",
+    gmail: "",
+    github: "https://www.youtube.com/@ytintech5468",
+    twitter: "https://www.youtube.com/@ytintech5468", keywords: [
+      "sports",
+      "basketballs"
+    ]
   },
   {
-    id: "54e0eccd-8f36-462b-b648a-8182611d9add",
+    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    image: "https://media.licdn.com/dms/image/D4D03AQGyzdmk2K4HCw/profile-displayphoto-shrink_200_200/0/1669194890962?e=2147483647&v=beta&t=F1QsYZoFTnGKDtias13R1mxPCvjMVqLIeF0lUZBu2Jw",
+    name: "Harshit Saini",
+    rating: {
+      followers: 4.5,
+      connections: 56
+    },
+    work: "Student at National Institute of Technology Hamirpur Una Himachal Pradesh India",
+
+    linkdin: "",
+    insta: "",
+    gmail: "",
+    github: "https://www.youtube.com/@ytintech5468",
+    twitter: "https://www.youtube.com/@ytintech5468", keywords: [
+      "tshirts",
+      "apparel",
+      "mens"
+    ]
+  },
+  {
+    id: "54e0eccd-8f36-462b-b68a-8182611d9add",
     image: "https://images.unsplash.com/photo-1499952127939-9bbf5af6c51c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
-    name: "r - Black",
+    name: "mishal",
     rating: {
       followers: 5,
       connections: 2197
@@ -219,9 +415,9 @@ const products = [
     ]
   },
   {
-    id: "3ebe75dc-64d2-4137-88460-1f5a963e534b",
+    id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
     image: "https://images.unsplash.com/photo-1504593811423-6dd665756598?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
-    name: "6 Piece Wate Set",
+    name: "Downy",
     rating: {
       followers: 4,
       connections: 37
@@ -239,9 +435,9 @@ const products = [
     ]
   },
   {
-    id: "8c9c52b5-5a194-4bcb-a5d1-158a74287c53",
+    id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
     image: "https://images.unsplash.com/photo-1599566150163-29194dcaad36?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
-    name: "6-Pien Bakeware Baking Set",
+    name: "peter",
     rating: {
       followers: 4.5,
       connections: 175
@@ -258,9 +454,9 @@ const products = [
     ]
   },
   {
-    id: "e43638ce-6aa0-4b85-b27f-e1d047eb678c6",
+    id: "e43638ce-6aa0-4b85-4b27f-e1d07eb678c6",
     image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww",
-    name: " Cotton Socks - 6 Pairs",
+    name: "Kraven",
     rating: {
       followers: 4.5,
       connections: 87
@@ -278,9 +474,9 @@ const products = [
     ]
   },
   {
-    id: "15b6fc6f-327a-4ec4-896f-4863349e85a3d",
+    id: "15b6fc6f-327a-4ec4-8946f-4864349e85a3d",
     image: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVyc29ufGVufDB8fDB8fHww",
-    name: "Interm Basketball",
+    name: "lewis",
     rating: {
       followers: 4,
       connections: 127
@@ -297,51 +493,9 @@ const products = [
     ]
   },
   {
-    id: "83d4ca15-0f35-498f5-b7a3-1ea210004f2e",
-    image: "https://images.unsplash.com/photo-1491349174775-aaafddd81942?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
-    name: "ACotton T-Shirt - 2 Pack",
-    rating: {
-      followers: 4.5,
-      connections: 56
-    },
-    work: "",
-
-    linkdin: "",
-    insta: "",
-    gmail: "",
-    github: "https://www.youtube.com/@ytintech5468",
-    twitter: "https://www.youtube.com/@ytintech5468", keywords: [
-      "tshirts",
-      "apparel",
-      "mens"
-    ],
-    type: "clothing",
-    sizeChartLink: "images/clothing-size-chart.png"
-  },
-  {
-    id: "54e0eccd-8f36-462b-b68a-8182611md9add",
-    image: "https://images.unsplash.com/photo-1499952127939-9bbf5af6c51c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
-    name: "2 Ster - Black",
-    rating: {
-      followers: 5,
-      connections: 2197
-    },
-    work: "",
-
-    linkdin: "",
-    insta: "",
-    gmail: "",
-    github: "https://www.youtube.com/@ytintech5468",
-    twitter: "https://www.youtube.com/@ytintech5468", keywords: [
-      "toaster",
-      "kitchen",
-      "appliances"
-    ]
-  },
-  {
-    id: "3ebe75dc-64d2-4137-8860-1f5a9m63e534b",
+    id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
     image: "https://images.unsplash.com/photo-1504593811423-6dd665756598?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
-    name: "6 Piner Plate Set",
+    name: "Downy",
     rating: {
       followers: 4,
       connections: 37
@@ -359,9 +513,9 @@ const products = [
     ]
   },
   {
-    id: "8c9c52b5-5a19-4bcb-a5d1-158ma74287c53",
+    id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
     image: "https://images.unsplash.com/photo-1599566150163-29194dcaad36?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
-    name: "6-Piece Nonstick",
+    name: "peter",
     rating: {
       followers: 4.5,
       connections: 175
@@ -376,6 +530,145 @@ const products = [
       "kitchen",
       "cookware"
     ]
+  },
+  {
+    id: "e43638ce-6aa0-4b85-4b27f-e1d07eb678c6",
+    image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww",
+    name: "Kraven",
+    rating: {
+      followers: 4.5,
+      connections: 87
+    },
+    work: "",
+
+    linkdin: "",
+    insta: "",
+    gmail: "",
+    github: "https://www.youtube.com/@ytintech5468",
+    twitter: "https://www.youtube.com/@ytintech5468", keywords: [
+      "socks",
+      "sports",
+      "apparel"
+    ]
+  },
+  {
+    id: "15b6fc6f-327a-4ec4-8946f-4864349e85a3d",
+    image: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVyc29ufGVufDB8fDB8fHww",
+    name: "lewis",
+    rating: {
+      followers: 4,
+      connections: 127
+    },
+    work: "",
+
+    linkdin: "",
+    insta: "",
+    gmail: "",
+    github: "https://www.youtube.com/@ytintech5468",
+    twitter: "https://www.youtube.com/@ytintech5468", keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },
+  {
+    id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+    image: "https://images.unsplash.com/photo-1504593811423-6dd665756598?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
+    name: "Downy",
+    rating: {
+      followers: 4,
+      connections: 37
+    },
+    work: "",
+
+    linkdin: "",
+    insta: "",
+    gmail: "",
+    github: "https://www.youtube.com/@ytintech5468",
+    twitter: "https://www.youtube.com/@ytintech5468", keywords: [
+      "plates",
+      "kitchen",
+      "dining"
+    ]
+  },
+  {
+    id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
+    image: "https://images.unsplash.com/photo-1599566150163-29194dcaad36?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
+    name: "peter",
+    rating: {
+      followers: 4.5,
+      connections: 175
+    },
+    work: "",
+
+    linkdin: "",
+    insta: "",
+    gmail: "",
+    github: "https://www.youtube.com/@ytintech5468",
+    twitter: "https://www.youtube.com/@ytintech5468", keywords: [
+      "kitchen",
+      "cookware"
+    ]
+  },
+  {
+    id: "e43638ce-6aa0-4b85-4b27f-e1d07eb678c6",
+    image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww",
+    name: "Kraven",
+    rating: {
+      followers: 4.5,
+      connections: 87
+    },
+    work: "",
+
+    linkdin: "",
+    insta: "",
+    gmail: "",
+    github: "https://www.youtube.com/@ytintech5468",
+    twitter: "https://www.youtube.com/@ytintech5468", keywords: [
+      "socks",
+      "sports",
+      "apparel"
+    ]
+  },
+  {
+    id: "15b6fc6f-327a-4ec4-8946f-4864349e85a3d",
+    image: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVyc29ufGVufDB8fDB8fHww",
+    name: "lewis",
+    rating: {
+      followers: 4,
+      connections: 127
+    },
+    work: "",
+
+    linkdin: "",
+    insta: "",
+    gmail: "",
+    github: "https://www.youtube.com/@ytintech5468",
+    twitter: "https://www.youtube.com/@ytintech5468", keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },
+  {
+    id: "83d4ca15-0f35-48f5-b7a3-14ea210004f2e",
+    image: "https://images.unsplash.com/photo-1491349174775-aaafddd81942?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
+    name: "Adon",
+    rating: {
+      followers: 4.5,
+      connections: 56
+    },
+    work: "",
+
+    linkdin: "",
+    insta: "",
+    gmail: "",
+    github: "https://www.youtube.com/@ytintech5468",
+    twitter: "https://www.youtube.com/@ytintech5468", keywords: [
+      "tshirts",
+      "apparel",
+      "mens"
+    ],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png"
   }
 ];
 export default products;
